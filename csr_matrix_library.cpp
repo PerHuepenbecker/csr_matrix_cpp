f6db314aee6352e33956bb61b9a8cd4d7d0ac4bd
perhuepenbecker <perhuepenbecker@googlemail.com>
2025-03-09 17:16:57 +0100

    added library files

---
+++ csr_matrix_library.cpp
@@ -0,0 +1,8 @@
+#include "csr_matrix_library.h"
+
+#include <iostream>
+
+
+void version(){
+    std::cout << "0.0.1" << std::endl;
+}
---
+++ csr_matrix_library.h
@@ -0,0 +1,7 @@
+#ifndef CSR_MATRIX_CPP_CSR_MATRIX_LIBRARY_H
+#define CSR_MATRIX_CPP_CSR_MATRIX_LIBRARY_H
+
+#include "matrix/Matrix.h"
+#include "csr/CSR_matrix.h"
+
+#endif //CSR_MATRIX_CPP_CSR_MATRIX_LIBRARY_H
