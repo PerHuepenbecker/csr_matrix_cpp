e5776ab5d373ae0977af74fa5770c026faf56e6a
perhuepenbecker <perhuepenbecker@googlemail.com>
2025-03-09 01:13:13 +0100

    fixed csr display method | bugfix csr-dense multiplication

--- csr/CSR_matrix.h
+++ csr/CSR_matrix.h
@@ -48,32 +48,67 @@ class CSRMatrix {
 
     // display method for the full matrix including all zero values
 
-    void display_full(){
-        for(size_t i = 0; i < rows_count; i++){
-
+    void display_full() {
+        for (size_t i = 0; i < rows_count; i++) {
             auto start_index = row_ptrs[i];
-            auto end_index = row_ptrs[i+1];
+            auto end_index = row_ptrs[i + 1];
 
             size_t k = start_index;
 
             for (size_t j = 0; j < cols_count; j++) {
-                if(j == col_indices[k]){
+                if (k < end_index && j == col_indices[k]) {
                     std::cout << data[k] << " ";
-                    if(k < end_index) k++;
-
+                    k++;  // Safely increment within bounds
                 } else {
                     std::cout << "0 ";
                 }
             }
-            std::cout<< std::endl;
+            std::cout << std::endl;
         }
     }
+
+    // Overloaded operator for scalar multiplication of a csr matrix
 
     void operator*(U scalar){
         for (auto& element: data){
             element *= scalar;
         }
     }
+
+    // Overloaded operator for matrix multiplication of a csr matrix with a dense
+    // matrix that returns a new Matrix object
+
+    friend Matrix<U> operator*(const CSRMatrix<U>& lhs, const Matrix<U>&rhs){
+
+        if(lhs.cols_count != rhs.get_rows_count()){
+            throw std::invalid_argument("Incompatible matrix dimensions for multiplication");
+        }
+
+        std::vector<U> result(lhs.rows_count * rhs.get_rows_count(), 0);
+
+        // First iterate over all rows in the lhs matrix. Standard for each matrix multiplication
+
+        for(size_t i = 0; i < lhs.rows_count; i++){
+
+            // Loop over the non-zero elements in the lhs csr matrix using the indexing of the row pointers
+
+            for(size_t k = lhs.row_ptrs[i]; k < lhs.row_ptrs[i+1]; k++){
+                size_t j = lhs.col_indices[k]; // column index of the non-zero row data
+                U value = lhs.data[k]; // actual value of the non-zero lhs element
+
+                // Iterate over the columns of the rhs matrix
+
+                for (size_t l = 0; l < rhs.get_cols_count(); l++){
+                    result[i*rhs.get_cols_count()+l] += value * rhs.get_data()[j * rhs.get_cols_count() + l];
+                }
+
+
+
+            }
+        }
+        return Matrix(result, rhs.get_cols_count());
+    }
+
 
 };
 
